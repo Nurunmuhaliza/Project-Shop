603e4570bdf465df04aa86564f7e40cb67524800
Nurunmuhaliza <nurunmuhaliza@gmail.com>
2022-10-22 09:26:23 +0700

    file baru semoga bisa

---
+++ shop-project/src/components/TopTitle.vue
@@ -0,0 +1,7 @@
+<template>
+    <nav class="navbar bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">SHOP</span>
+    <span class="navbar-brand mb-0 h1">Cart ({{ totalQty }})</span>
+  </div>
+</nav>
